f0c7a1508efa13f20f01417af6533fc48e50c8aa
Diogo Fonseca <diogo.fonseca@maxxidata.com>
2020-04-27 03:24:34 -0300

    added bootstrap dep with custom theme

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'bootswatch/dist/sketchy/bootstrap.min.css'
+
 import Routes from './routes/Routes'
 
 ReactDOM.render(<Routes />, document.getElementById('root'))
